c4cb37b130037d632edea61870ddb85a345c1422
Stefan <stadolf@gmail.com>
2012-11-30 20:28:43 +0100

    using customized bootstrap typeahead with delay option jumping to map result, storing map marker locaiton removed translation properties / preview controls

--- src/jquery.addresspicker.js
+++ src/jquery.addresspicker.js
@@ -33,6 +33,7 @@
         },
 
         updatePosition: function() {
+          this.gmarker.setVisible(true);
           this._updatePosition(this.gmarker.getPosition());
         },
     
@@ -48,73 +49,66 @@
     
         _create: function() {
           this.geocoder = new google.maps.Geocoder();
+
+          var self = this;
+          this.$element.on("changeAddress", this._selectAddress.bind(this));
           this.$element.typeahead({
               minLength:3,
-              source: this._geocode.bind(this)
-              ,textproperty: function(item) {
+              delay:1000,
+              source: this._geocode.bind(this),
+              textproperty: function(item) {
                   return item.formatted_address;
               }, updater: function(item) {
-                  console.dir(item);
+                  self.$element.trigger('changeAddress',item);
                   return this.textproperty(item);
+              }, matcher: function(item) {
+                  return true;
               }
           });
 
-            if (this.options.elements.map) {
-                this.mapElement = $(this.options.elements.map);
+            if (this.options.map) {
+                this.$mapElement = $(this.options.map)[0];
                 this._initMap();
             }
-
-          this.lat      = $(this.options.elements.lat);
-          this.lng      = $(this.options.elements.lng);
-          this.locality = $(this.options.elements.locality);
-          this.administrative_area_level_2 = $(this.options.elements.administrative_area_level_2);
-          this.administrative_area_level_1 = $(this.options.elements.administrative_area_level_1);
-          this.country  = $(this.options.elements.country);
-          this.postal_code = $(this.options.elements.postal_code);
-          this.type     = $(this.options.elements.type);
-
 
         },
 
         _initMap: function() {
-          if (this.lat && this.lat.val()) {
-            this.options.mapOptions.center = new google.maps.LatLng(this.lat.val(), this.lng.val());
-          }
 
-          this.gmap = new google.maps.Map(this.mapElement[0], this.options.mapOptions);
+          this.gmap = new google.maps.Map(this.$mapElement, this.options.mapOptions);
           this.gmarker = new google.maps.Marker({
             position: this.options.mapOptions.center,
             map:this.gmap,
             draggable: this.options.draggableMarker});
+
           google.maps.event.addListener(this.gmarker, 'dragend', $.proxy(this._markerMoved, this));
+
           this.gmarker.setVisible(false);
-        },
-    
-        _updatePosition: function(location) {
-          if (this.lat) {
-            this.lat.val(location.lat());
-          }
-          if (this.lng) {
-            this.lng.val(location.lng());
-          }
+          this.$element.on("changeAddress", this._focusAddress.bind(this));
         },
     
         _markerMoved: function() {
-          this._updatePosition(this.gmarker.getPosition());
+          var markerPos= this.gmarker.getPosition();
+          this.geocoder.geocode({latLng: markerPos}, function( results, status) {
+             if(status == google.maps.GeocoderStatus.OK) {
+                 console.dir(results);
+             }
+          });
+          this._selectAddress(null, markerPos);
         },
 
         // Autocomplete source method: fill its suggests with google geocoder results
         _geocode: function(query, process) {
-            self = this;
             this.geocoder.geocode({
-                'address': query + this.options.appendAddressString,
+                'address': query+" ",
                 'region': this.options.regionBias
             }, function(results, status) {
                 if (status == google.maps.GeocoderStatus.OK) {
                     var suggestions = [];
                     for (var i = 0; i < results.length; i++) {
-                        suggestions.push(results[i]); //
+                        suggestions.push(results[i]);
                     };
+                    console.log(status);
                 }
                 process(suggestions);
             })
@@ -130,85 +124,57 @@
           return false;
         },
 
-        _focusAddress: function(event, ui) {
-          var address = ui.item;
-          if (!address) {
+        _focusAddress: function(evt,gMapsLocation) {
+          if (!gMapsLocation) {
             return;
           }
 
           if (this.gmarker) {
-            this.gmarker.setPosition(address.geometry.location);
+            this.gmarker.setPosition(gMapsLocation.geometry.location);
             this.gmarker.setVisible(true);
+            this.gmap.fitBounds(gMapsLocation.geometry.viewport);
+          }
 
-            this.gmap.fitBounds(address.geometry.viewport);
-          }
-          this._updatePosition(address.geometry.location);
-
-          if (this.locality) {
-            this.locality.val(this._findInfo(address, 'locality'));
-          }
-          if (this.administrative_area_level_2) {
-            this.administrative_area_level_2.val(this._findInfo(address, 'administrative_area_level_2'));
-          }
-          if (this.administrative_area_level_1) {
-            this.administrative_area_level_1.val(this._findInfo(address, 'administrative_area_level_1'));
-          }
-          if (this.country) {
-            this.country.val(this._findInfo(address, 'country'));
-          }
-          if (this.postal_code) {
-            this.postal_code.val(this._findInfo(address, 'postal_code'));
-          }
-          if (this.type) {
-            this.type.val(address.types[0]);
-          }
         },
-
-        _selectAddress: function(event, ui) {
-          this.selectedResult = ui.item;
+        _selectAddress: function(evt, gMapsLocation) {
+            this.selectedResult = gMapsLocation;
         }
 
       };
 
-    $.fn.addresspicker = function (option) {
+    $.fn.addresspicker = function (option, val) {
         return this.each(function () {
             var $this = $(this)
             var data = $this.data('addresspicker')
-            var options = $.extend({}, $.fn.addresspicker.defaults, $this.data(), typeof option == 'object' && option)
+            var options = $.extend(true, {}, $.fn.addresspicker.defaults, $this.data(), typeof option == 'object' && option)
             if (!data)
                 $this.data('addresspicker', (data = new AddressPicker(this, options)));
 
             //API:
             if (typeof option == 'string') //activate methods
-                data[option]()
+                data[option](val)
         });
     }
 
     $.fn.addresspicker.defaults =  {
-        appendAddressString: "",
         draggableMarker: true,
         regionBias: null,
+        map: false,
+        typeaheaddelay: 1000,
         mapOptions: {
             zoom: 5,
             center: new google.maps.LatLng(46, 2),
             scrollwheel: false,
             mapTypeId: google.maps.MapTypeId.ROADMAP
-        },
-        elements: {
-            map: false,
-            lat: false,
-            lng: false,
-            locality: false,
-            administrative_area_level_2: false,
-            administrative_area_level_1: false,
-            country: false,
-            postal_code: false,
-            type: false
-
         }
+
     };
 
     $.fn.addresspicker.Constructor = AddressPicker;
+
+    $.fn.addresspicker.position = function () {
+        return this.selectedResult;
+    }
 
     /*$.extend( $.addresspicker, {
         version: "@VERSION"
